68c43ed66ea5462e014c16b319da3e0bad8a2c8e
unknown <Mukizamaximus09@gmail.com>
2025-05-15 23:25:51 +0200

    keept it simple and add the lab role as arn since its the only role i have access too damn amazon

--- Docker+aurora/Terraform/ecs.tf
+++ Docker+aurora/Terraform/ecs.tf
@@ -9,7 +9,7 @@ resource "aws_ecs_task_definition" "app" {
   requires_compatibilities = ["FARGATE"]
   cpu                      = var.app_cpu
   memory                   = var.app_memory
-  execution_role_arn = "arn:aws:iam::125755581655:role/voclabs"
+  execution_role_arn = "arn:aws:iam::125755581655:role/LabRole"
 
   container_definitions = jsonencode([
     {
